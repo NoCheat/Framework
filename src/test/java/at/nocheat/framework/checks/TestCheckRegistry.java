7f0df579e37c01f39dafc8de43e9ccef2d990af2
asofold <asofold@web.de>
2013-09-21 18:23:28 +0200

    More on tests.

--- src/test/java/at/nocheat/framework/checks/TestCheckRegistry.java
+++ src/test/java/at/nocheat/framework/checks/TestCheckRegistry.java
@@ -1,5 +1,7 @@
 package at.nocheat.framework.checks;
 
+import static org.junit.Assert.fail;
+
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -33,9 +35,34 @@ public class TestCheckRegistry {
 	@Test
 	public void registerRootTypes() throws NotRegisteredException, IllegalOverrideException {
 		CheckTypeRegistry reg = new CheckTypeRegistry();
-		for (String name : Arrays.asList("Test", "Test2", "Test3")) {
+		for (String name : Arrays.asList("Test1", "Test2", "Test3")) {
 			reg.registerCheckType(name, null);
 		}
 	}
+	
+	@Test
+	public void registerParents() throws NotRegisteredException, IllegalOverrideException {
+		CheckTypeRegistry reg = new CheckTypeRegistry();
+		final String[] names = new String[]{"Test1", "Test2", "Test3", "Test4"};
+		CheckType last = null;
+		String expectedName = names[0];
+		for (int i = 0; i < names.length; i++) {
+			String name = names[i];
+			last = reg.registerCheckType(name, last);
+			if (i > 0) expectedName += "." + name;
+		}
+		if (!last.getFullName().equals(expectedName)) {
+			fail("Expect full name to be " + expectedName + " , got instead: " + last.getFullName());
+		}
+	}
+	
+	@Test(expected=NotRegisteredException.class)
+	public void registerWrongParent() throws NotRegisteredException, IllegalOverrideException {
+		CheckType notRegistered = new CheckTypeRegistry().registerCheckType("NotRegistered", null);
+		CheckTypeRegistry reg = new CheckTypeRegistry();
+		reg.registerCheckType("Test", notRegistered);
+	}
+	
+	// TODO: More on getCheckType, getCheckTypeExact, isValid
 	
 }
